260e0d98e02ac5880029827b3746c5ecb7268ab9
Sharad Bhat <sharad.mbhat@gmail.com>
2018-03-11 02:07:11 +0530

    Added merge sort

--- Sorting/sort-export.js
+++ Sorting/sort-export.js
@@ -1,11 +1,13 @@
 const selectionSort = require('./selection-sort.js');
 const bubbleSort = require('./bubble-sort.js');
 const insertionSort = require('./insertion-sort.js');
+const mergeSort = require('./merge-sort.js');
 
 exports = {
   selectionSort,
   bubbleSort,
-  insertionSort
+  insertionSort,
+  mergeSort
 };
 
 module.exports = exports;
